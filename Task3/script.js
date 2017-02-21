680a63411285dc301dc0504254eb36fe9fd87ed4
dedn <yura.kirillov@mev.com>
2017-02-21 10:22:42 +0200

    completed the work on the task 3. Change the server node js . Added loading through. added to the counter.

--- Task3/script.js
+++ Task3/script.js
@@ -1,12 +1,50 @@
-function getProducts() {
+(function () {
+
+  var buttons = document.querySelector('#list');
+
+  buttons.onclick = function (e) {
+    var target = e.target;
+    if (target.tagName != 'BUTTON') return;
+    var product = target.closest('li').querySelector('.product-item__title').textContent;
+    alert(' You have bought a ' + product);
+    var count = target.closest('li').querySelector('.product-item__counter');
+    count.innerText = parseInt(count.innerText) + 1;
+  };
+
+  var container = document.querySelector(".products-list");
+  getProduct();
+
+  function renderProduct(item) {
+    item.forEach(function (product) {
+      var element = getElementFromTemplate(product);
+      container.appendChild(element);
+    });
+  }
+
+  function getProduct() {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/test", false);
+    xhr.open('GET', 'data/product.json');
+    xhr.onload = function (evt) {
+      var rawData = evt.target.response;
+      var loadedProduct = JSON.parse(rawData);
+      renderProduct(loadedProduct);
+    };
+
     xhr.send();
-    if (xhr.status != 200) {
-        alert(xhr.status + ": " + xhr.statusText);
+  }
+
+  function getElementFromTemplate(data) {
+    var template = document.querySelector('#template');
+    if ('content' in template) {
+      var element = template.content.children[0].cloneNode(true);
     } else {
-        alert(xhr.responseText);
+      var element = template.children[0].cloneNode(true);
     }
 
-}
-getProducts();
+    element.querySelector('.product-item__title').textContent = data.name;
+    element.querySelector('.content').textContent = data.description;
+
+    return element;
+
+  }
+})();
--- Task3/server.js
+++ Task3/server.js
@@ -1,33 +1,9 @@
-var http = require("http");
-var url = require("url");
+var http = require('http');
+var static = require('node-static');
+var file = new static.Server('.');
 
-function start(route, handle) {
-    function onRequest(request, response) {
-        var postData = "";
-        var pathname = url.parse(request.url).pathname;
-        console.log("Request for " + pathname + " received.");
+http.createServer(function(req, res) {
+  file.serve(req, res);
+}).listen(8080);
 
-        request.setEncoding("utf8");
-
-        request.addListener("data", function (postDataChunk) {
-            postData += postDataChunk;
-            console.log("Received POST data chunk '" +
-                postDataChunk + "'.");
-        });
-
-        request.addListener("end", function () {
-            route(handle, pathname, response, postData);
-        });
-
-    }
-
-    http.createServer(onRequest).listen(8888);
-    console.log("Server has started.");
-
-}
-
-exports.start = start;
-
-
-
-
+console.log('Server running on port 8080');
