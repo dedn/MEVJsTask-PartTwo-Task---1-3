64ba8a4ee1e218254e8af38d4346fff8c6073544
dedn <yura.kirillov@mev.com>
2017-02-27 17:11:28 +0200

    added new task

---
+++ Task2-3v2/server.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var data = fs.readFileSync('website/data/product.json');
+var product = JSON.parse(data);
+console.log(product);
+
+
+console.log('start');
+var express = require('express');
+var app = express();
+var server;
+server = app.listen(3001, listening);
+function listening() {
+  console.log("listening...");
+}
+app.use(express.static('website'));
+
+
+app.get('/', addValue);
+
+function addValue(request, response) {
+  var values = target.closest('li').querySelector('.product-item__counter').textContent;
+  var data = request.params;
+  var value = Number(data.value);
+  var reply;
+
+  values[value] = value;
+
+  var data = JSON.stringify(values, null, 2);
+  fs.writeFile('website/data/product.json', data, finished);
+
+  response.send(reply);
+}
